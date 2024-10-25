c84befaf3b6f9cea5ab013ce768dba00fc385dde
hema <bhemavallika@gmail.com>
2024-10-25 08:53:36 +0530

    updated

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import Productcard from '../components/Productcard'
+import ProductCard from '../components/ProductCard'
 const Shop = () => {
     const products = useSelector(state => state.product)
   return (
@@ -8,7 +8,7 @@ const Shop = () => {
     <h2 className="text-2xl font-bold mb-6 text-center shadow-md">Shop</h2>
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 cursor-pointer ">
       {products.products.map(((product) => (
-        <Productcard product={product}/>
+        <ProductCard product={product}/>
       )))}
     </div>
   </div>
